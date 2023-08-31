7faa3d48e33444e8f284b97c649dce2e3733008b
Beebake8848 <kbibek448@gmail.com>
2023-09-01 00:19:57 +0545

    adding

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
-from django.contrib import admin
-from django.urls import path
 
+from django.urls import path
+# from accounts.views import login_page , register_page
+from accounts import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('login/',views.login_page , name="login"),
+    path('register/',views.register_page , name="register")
+
+
 ]
